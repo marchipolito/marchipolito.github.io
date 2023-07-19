97b022996e9283524241e0173f1d6c9c18f32f57
Marc <marc@marcs-mbp-2.lan>
2023-07-19 11:33:56 -0700

    Updates

--- src/components/old/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-import Banner from "./Banner";
-
-const Home = () => {
-  return <Banner color>foo</Banner>;
-};
-
-export default Home;
